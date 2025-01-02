566707389e65b0a18b43a7f763059ad38e26da00
Prabh ‪‪ <singh20prabh@gmail.com>
2025-01-02 11:18:22 +0530

    changes in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,12 +8,26 @@ function Header() {
         <div className="container flexSB">
           <nav className="flexSB">
             <div className="logo">
-              <img src="" alt="" />
+              <img src="./images/logo.png" alt="company logo streamIt" />
             </div>
             <ul className="flexSB">
               <Link to="/">Home</Link>
+              <Link to="/series">Series</Link>
+              <Link to="/movies">Movies</Link>
+              <Link to="/pages">Pages</Link>
+              <Link to="/pricing">Pricing</Link>
+              <Link to="/contact">Contact</Link>
             </ul>
+            <button className="toggle">
+              <i className="fa fa-bars"></i>
+            </button>
           </nav>
+          <div className="account flexSB">
+            <i className="fa fa-search"></i>
+            <i className="fa fa-bell"></i>
+            <i className="fa fa-user"></i>
+            <button>Subscribe Now</button>
+          </div>
         </div>
       </header>
     </>
